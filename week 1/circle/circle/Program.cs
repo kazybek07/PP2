392199a04311ca3dfda9a46cab4143e378df33c5
Kazybek <kazybek.07@gmail.com>
2018-01-24 11:03:43 +0600

    asd

---
+++ week 1/circle/circle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{   
+    class circle
+    {
+        public int radius;
+        public double area;
+        public double circum;
+        public int diameter;
+
+        public circle(int r)
+        {
+            radius = r;
+        }
+
+        public void findArea()
+        {
+            area = Math.PI * radius * radius;
+        }
+
+        public void findCircum()
+        {
+            circum = 2 * Math.PI * radius;
+        }
+
+        public void findDiameter()
+        {
+            diameter = 2 * radius;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circle c = new circle(3);
+            c.findArea();
+            c.findCircum();
+            c.findDiameter();
+
+            Console.WriteLine("area:" + c.area);
+            Console.WriteLine("circumference:"+ c.circum);
+            Console.WriteLine("diameter:" + c.diameter);
+
+            Console.ReadKey();
+
+        }
+    }
+}
