813845517a906d2c59f73da0cca5d3928cdf8270
Kazybek <kazybek.07@gmail.com>
2018-01-24 11:13:21 +0600

    asd

---
+++ week 1/rectangle/rectangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle
+{   
+    class rectangle
+    {
+        public int width;
+        public int height;
+        public double area;
+        public double per;
+
+        public rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public void findArea()
+        {
+            area = width * height;
+        }
+        public void findPer()
+        {
+            per = (width + height) * 2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle(5, 6);
+            r.findArea();
+            r.findPer();
+
+            Console.WriteLine("area:" + r.area);
+            Console.WriteLine("perimeter:" + r.per);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week 1/student/student/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{      
+    class student
+    {
+        public string name;
+        public string sname;
+        public double gpa;
+
+        public student()
+        {
+            name = "Marat";
+            sname = "Gaziz";
+            gpa = 3.7;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + sname + " " + gpa;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+        }
+    }
+}
